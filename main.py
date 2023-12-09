d2f9693175f2bbe830391e60cc57f384c37e25ee
Sergey Igumnov <sergey.igymn@gmail.com>
2023-12-09 23:19:58 +0300

    Закончил на сегодня, пошел спать

--- main.py
+++ main.py
@@ -1,37 +1,71 @@
 from abc import ABC, abstractmethod
 import vk_api
 import selenium
+import pprint
 
 from config import VK_LOGIN, VK_PASSWORD
+
+MY_VK_ID = "56757868"
 
 
 class AuthException(Exception):
     pass
 
 
+class GetUserExeption(Exception):
+    pass
+
+
 class AbstractParser(ABC):
     @abstractmethod
-    def parse_one(self):
+    def parse_one(self, *args, **kwargs):
         ...
 
 
 class VkParser(AbstractParser):
     def __init__(self, login, password):
         vk_session = vk_api.VkApi(login=VK_LOGIN, password=VK_PASSWORD, app_id=2685278)
-        vk_session.auth()
+        try:
+            vk_session.auth()
+        except Exception:
+            raise AuthException()
 
         self.vk = vk_session.get_api()
-        self.users = []
-        print(self.vk.wall.post(message='Hello world!'))
+        self.users = {}
 
-    def parse_one(self):
-        pass
+    def parse_one(self, vk_id):
+        self._check_if_user_exists(vk_id=vk_id)
 
+        groups_ids = self._parse_one_person_groups(vk_id=vk_id)
+        response = self._get_groups_description(groups_ids)
+        # pprint.pprint(response)
+
+    def _parse_one_person_groups(self, vk_id):
+        response = self.vk.groups.get(user_id=vk_id)
+        groups = response["items"]
+        return groups
+
+    def _get_groups_description(self, groups_ids: []):
+        response = self.vk.groups.getById(group_ids=groups_ids, fields="name, description")
+        print(response)
+        groups_description = {}
+        for group in response:
+            id = group["id"]
+            name = group["name"]
+            description = group["description"]
+            groups_description[id] = {"name": name, "description": description}
+        return groups_description
+
+    def _check_if_user_exists(self, vk_id):
+        try:
+            self.vk.users.get(user_ids=vk_id)
+        except Exception:
+            raise GetUserExeption()
 
 
 def main():
     vk_parser = VkParser(login=VK_LOGIN, password=VK_PASSWORD)
-    vk_parser.parse_one()
+    vk_parser.parse_one(vk_id=MY_VK_ID)
 
 
 if __name__ == "__main__":
