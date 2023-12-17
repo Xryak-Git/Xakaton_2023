636b191e3900cd7ab267a828ca2d5e11b4fa8e5b
Xryak <sergey.igymn@gmail.com>
2023-12-17 18:35:39 +0300

    last changes

--- scripts/main.py
+++ scripts/main.py
@@ -9,6 +9,7 @@ from config import VK_LOGIN, VK_PASSWORD
 
 MY_VK_ID = "56757868"
 OUTPUT_PATH = r"../output/"
+TOKEN = "vk1.a.3th8ECvivwyx86YdlsIL69Fdkj2vITUKzATtXJmip0or7v-NRFYdh9lYaPPTCzFH2nnL2RS0ldQMQpQgvCYEsIgYJnSgfNtpcTNrZ5d0rZoarIoWMVjfDt2Ingj2-hw-Y3eCACUFl51ivFSAY2AgV6wEAZXM1dT03WkotoX_pBAiDIZ_IZ0CTeWvXi17-CSpaOSPSRF4gsm_4afFkCIwFw&expires_in=86400"
 
 class AuthException(Exception):
     pass
@@ -31,15 +32,53 @@ class Writer:
             json.dump(data, file, ensure_ascii=False, indent=4)
 
 
+class Database:
+    def __init__(self, name):
+        self._conn = sqlite3.connect(OUTPUT_PATH + name)
+        self._cursor = self._conn.cursor()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    @property
+    def connection(self):
+        return self._conn
+
+    @property
+    def cursor(self):
+        return self._cursor
+
+    def commit(self):
+        self.connection.commit()
+
+    def close(self, commit=True):
+        if commit:
+            self.commit()
+        self.connection.close()
+
+    def execute(self, sql, params=None):
+        self.cursor.execute(sql, params or ())
+
+    def fetchall(self):
+        return self.cursor.fetchall()
+
+    def fetchone(self):
+        return self.cursor.fetchone()
+
+    def query(self, sql, params=None):
+        self.cursor.execute(sql, params or ())
+        return self.fetchall()
 
 
 class VkParser(AbstractParser):
     def __init__(self, login, password):
-        vk_session = vk_api.VkApi(login=VK_LOGIN, password=VK_PASSWORD, app_id=2685278)
-        try:
-            vk_session.auth()
-        except Exception:
-            raise AuthException()
+        vk_session = vk_api.VkApi(login=VK_LOGIN, token=TOKEN, app_id=2685278)
+
+        vk_session.auth(token_only=True)
+
 
         self.vk = vk_session.get_api()
         self.users = {}
@@ -59,6 +98,7 @@ class VkParser(AbstractParser):
 
     def _get_groups_description(self, groups_ids: []):
         response = self.vk.groups.getById(group_ids=groups_ids, fields="name, description")
+        print(response)
         groups_description = {}
         for group in response:
             id = group["id"]
@@ -78,8 +118,7 @@ def main():
     vk_parser = VkParser(login=VK_LOGIN, password=VK_PASSWORD)
     data = vk_parser.parse_one(vk_id=MY_VK_ID)
 
-    Writer.json(data=data)
-
+    #db = Database(name="vk_users.db")
 
 if __name__ == "__main__":
     main()
